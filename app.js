f620d22b4b97870e39fad10e5010d5c30393543b
Amit Triffon <amit@ronin.co.il>
2016-02-23 21:10:47 +0200

    test commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 80, function () {
+app.listen(process.env.PORT || 81, function () {
     console.log('server started at port %s. http://localhost:%s', this.address().port, this.address().port);
 });
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@
 var $ = require('jquery');
 var bootstrp = require('bootstrap');
 var angular = require('angular');
+var _ = require('lodash');
 
 angular.module('ngLpFormGenerator', [])
 
 .controller('Ctrl', function ($scope) {
   this.title = 'Form';
   this.model = require('../models/contact.json');
+
+
+  this.fields = this.model.properties;
+  this.relations = this.model.relations;
+
+
+
 
   this.data = {};
   this.data.name = 'Amit Triffon';
